0ed886d3c14be88c3519fde05bffe33767a4c162
D-Brox <davidfbg00@gmail.com>
2025-12-07 14:15:04 -0300

    day7

---
+++ src/days/day07.rs
@@ -0,0 +1,39 @@
+use std::collections::HashSet;
+
+pub fn solution1(input: Vec<String>) -> usize {
+    let mut splits = 0;
+    let mut tachyons = HashSet::new();
+    tachyons.insert(input[0].find('S').unwrap());
+    for i in 1..input.len() {
+        tachyons = tachyons
+            .into_iter()
+            .flat_map(|t| {
+                if input[i].chars().nth(t).unwrap() == '^' {
+                    splits += 1;
+                    vec![t - 1, t + 1]
+                } else {
+                    vec![t]
+                }
+            })
+            .collect();
+    }
+    splits
+}
+
+pub fn solution2(input: Vec<String>) -> usize {
+    let mut tachyons = vec![0; input[0].len()];
+    tachyons[input[0].find('S').unwrap()] = 1;
+    for i in 1..input.len() {
+        let mut after = vec![0; input[0].len()];
+        for (j, (c, t)) in input[i].chars().zip(&tachyons).enumerate() {
+            if c == '^' {
+                after[j - 1] += t;
+                after[j + 1] += t;
+            } else {
+                after[j] += t;
+            }
+        }
+        std::mem::swap(&mut tachyons, &mut after);
+    }
+    tachyons.iter().sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
+use seq_macro::seq;
+seq!( N in 01..=07 {
+
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::{Path, PathBuf};
 
 use clap::{Parser, Subcommand, ValueEnum};
-use seq_macro::seq;
-
-mod days {
-    use seq_macro::seq;
-
-    seq!( N in 01..=06 {
-        pub mod day~N;
-    });
-}
-use days::*;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
@@ -35,36 +28,37 @@ enum Part {
     Part2,
 }
 
-seq!( N in 01..=06 {
-    #[derive(Subcommand)]
-    enum Commands {
-        #(
-            Day~N{
-                #[arg(value_enum)]
-                part: Part,
-                #[arg(short, long, value_name = "FILE")]
-                input: PathBuf,
-            },
-        )*
-    }
-});
+
+mod days {
+    #(pub mod day~N;)*
+}
+
+#[derive(Subcommand)]
+enum Commands {#(
+    Day~N{
+        #[arg(value_enum)]
+        part: Part,
+        #[arg(short, long, value_name = "FILE")]
+        input: PathBuf,
+    },
+)*}
+
+use days::*;
 
 fn main() {
     let cli = Cli::parse();
-    seq!( N in 01..=06 {
-        match &cli.command {
-            #(
-                Commands::Day~N{part,input} => {
-                    let input:Vec<String> = read_lines(input)
-                        .unwrap()
-                        .map(|l|l.unwrap())
-                        .collect();
-                    match part{
-                        Part::Part1 => println!("{:?}",day~N::solution1(input)),
-                        Part::Part2 => println!("{:?}",day~N::solution2(input)),
-                    }
-                },
-            )*
-        }
-    });
+        match &cli.command {#(
+            Commands::Day~N{part,input} => {
+                let input:Vec<String> = read_lines(input)
+                    .unwrap()
+                    .map(|l|l.unwrap())
+                    .collect();
+                match part{
+                    Part::Part1 => println!("{:?}",day~N::solution1(input)),
+                    Part::Part2 => println!("{:?}",day~N::solution2(input)),
+                }
+            },
+        )*}
 }
+
+});
