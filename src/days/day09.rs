eee8b8f2183251edacc8654c18f45abf380082cf
D-Brox <davidfbg00@gmail.com>
2025-12-09 21:58:27 -0300

    day9 optimization

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,9 +1,7 @@
-use std::collections::{HashMap, HashSet};
-
 use itertools::Itertools;
 
-pub fn part1(input: Vec<String>) -> i64 {
-    let pairs: Vec<(i64, i64)> = input
+pub fn part1(input: Vec<String>) -> usize {
+    let pairs: Vec<(usize, usize)> = input
         .iter()
         .map(|l| {
             l.split(',')
@@ -17,7 +15,11 @@ pub fn part1(input: Vec<String>) -> i64 {
         let [&p1, &p2] = pair[0..2] else {
             unreachable!()
         };
-        max = max.max(((p1.0 - p2.0).abs() + 1) * ((p1.1 - p2.1).abs() + 1));
+        let x1 = p1.0.min(p2.0);
+        let x2 = p1.0.max(p2.0);
+        let y1 = p1.1.min(p2.1);
+        let y2 = p1.1.max(p2.1);
+        max = max.max((x2 - x1 + 1) * (y2 - y1 + 1));
     }
     max
 }
@@ -33,90 +35,38 @@ pub fn part2(input: Vec<String>) -> usize {
         })
         .collect_vec();
 
-    // Coordinate compression
-    let map_x = red
-        .iter()
-        .map(|&(x, _)| x)
-        .dedup()
-        .sorted()
-        .enumerate()
-        .map(|(i, n)| (n, i + 1))
-        .collect::<HashMap<_, _>>();
-    let rev_x = map_x.iter().map(|(k, v)| (v, k)).collect::<HashMap<_, _>>();
-    let map_y = red
-        .iter()
-        .map(|&(_, y)| y)
-        .dedup()
-        .sorted()
-        .enumerate()
-        .map(|(i, n)| (n, i + 1))
-        .collect::<HashMap<_, _>>();
-    let rev_y = map_y.iter().map(|(k, v)| (v, k)).collect::<HashMap<_, _>>();
-    let mapped_red = red.iter().map(|(x, y)| (map_x[x], map_y[y])).collect_vec();
-
-    let mut max_i = 0;
-    let mut max_j = 0;
-    let mut color = HashSet::new();
-    // Fill border
-    for (k, (x, y)) in mapped_red.iter().enumerate() {
-        let prev = mapped_red[(k + 1) % mapped_red.len()];
-        if prev.0 == *x {
-            let pair = [y, &prev.1];
-            let [y1, y2] = pair.iter().sorted().collect_array().unwrap();
-            max_i = max_i.max(*x);
-            max_j = max_j.max(**y2);
-            (**y1..=**y2).for_each(|y| {
-                color.insert((*x, y));
-            });
-        } else {
-            let pair = [x, &prev.0];
-            let [x1, x2] = pair.iter().sorted().collect_array().unwrap();
-            max_i = max_i.max(**x2);
-            max_j = max_j.max(*y);
-            (**x1..=**x2).for_each(|x| {
-                color.insert((x, *y));
-            });
-        }
-    }
-
-    // Fill the inside
-    for j in 1..=max_j {
-        let mut inside = false;
-        let mut up = false;
-        for i in 1..=max_i {
-            if mapped_red.contains(&(i, j)) {
-                if !inside {
-                    up = color.contains(&(i, j - 1));
-                    inside = true;
-                } else if (color.contains(&(i, j - 1)) && up && !color.contains(&(i, j + 1)))
-                    || (!up && !color.contains(&(i, j - 1)))
-                {
-                    inside = false;
-                }
-            } else if color.contains(&(i, j)) {
-                inside = !inside;
-            } else if inside {
-                color.insert((i, j));
-            }
-        }
-    }
-
-    // Check all
     let mut max = 0;
-    for pair in mapped_red.iter().combinations(2) {
+    for pair in red.iter().combinations(2) {
         let [p1, p2] = pair[0..2] else { unreachable!() };
-        let pair = [p1.0, p2.0];
-        let [x1, x2] = pair.iter().sorted().collect_array().unwrap();
-        let pair = [p1.1, p2.1];
-        let [y1, y2] = pair.iter().sorted().collect_array().unwrap();
-        if (*x1..=*x2)
-            .cartesian_product(*y1..=*y2)
-            .all(|(x, y)| color.contains(&(x, y)))
-        {
-            let x1 = rev_x[x1];
-            let x2 = rev_x[x2];
-            let y1 = rev_y[y1];
-            let y2 = rev_y[y2];
+        let x1 = p1.0.min(p2.0);
+        let x2 = p1.0.max(p2.0);
+        let y1 = p1.1.min(p2.1);
+        let y2 = p1.1.max(p2.1);
+        // Logic inspired by the Sutherland–Hodgman algorithm
+        if red.iter().enumerate().all(|(k, &(x, y))| {
+            match (((x1 + 1)..x2).contains(&x), ((y1 + 1)..y2).contains(&y)) {
+                (true, true) => false, // Point inside
+                (true, false) => {
+                    let (nx, ny) = red[(k + 1) % red.len()];
+                    if x == nx {
+                        let range = y.min(ny)..=y.max(ny);
+                        // Check if line crosses horizontally
+                        return !(y1 != y2 && range.contains(&y1) && range.contains(&y2));
+                    }
+                    true
+                }
+                (false, true) => {
+                    let (nx, ny) = red[(k + 1) % red.len()];
+                    if y == ny {
+                        let range = x.min(nx)..=x.max(nx);
+                        // Check if line crosses vertically
+                        return !(x1 != x2 && range.contains(&x1) && range.contains(&x2));
+                    }
+                    true
+                }
+                (false, false) => true, // Completely outside
+            }
+        }) {
             max = max.max((y2 - y1 + 1) * (x2 - x1 + 1));
         }
     }
